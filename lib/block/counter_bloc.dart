50e67b850abcc7e0bf2d810b214935ebbc684b64
Ziad-Yaseen <ziadyaseen.dev@gmail.com>
2026-09-23 07:50:06 +0300

    add states and events and bloc

---
+++ lib/block/counter_bloc.dart
@@ -0,0 +1,20 @@
+import 'package:bloc_example_project/block/counter_events.dart';
+import 'package:bloc_example_project/block/counter_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterBloc extends Bloc<CounterEvents, CounterStates> {
+  CounterBloc() : super(const CounterState(0)) {
+    on<Increment> (_incrementCounter);
+    on<Decrement> (_decrementCounter);
+  }
+
+  void _incrementCounter(CounterEvents event, Emitter<CounterStates> emit) {
+    final currentState = state as CounterState;
+    emit(CounterState(currentState.counter + 1));
+  }
+
+  void _decrementCounter(CounterEvents event, Emitter<CounterStates> emit) {
+    final currentState = state as CounterState;
+    emit(CounterState(currentState.counter + 1));
+  }
+}
---
+++ lib/block/counter_states.dart
@@ -0,0 +1,15 @@
+import 'package:equatable/equatable.dart';
+
+abstract class CounterStates extends Equatable {
+  const CounterStates();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class CounterState extends CounterStates {
+  final int counter;
+  const CounterState(this.counter);
+
+
+}
